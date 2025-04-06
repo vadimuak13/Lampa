927698d65c21e5291a0aff32f8cb502b6909b96c
vadimuak13 <vadim.uak@gmail.com>
2025-04-06 15:40:06 +0300

    Add files via upload

---
+++ eneyida.js
@@ -0,0 +1,46 @@
+
+(function(){
+    let eneyida = {};
+
+    eneyida.search = function(query, call) {
+        let url = `https://eneyida.tv/index.php?do=search&subaction=search&story=${encodeURIComponent(query)}`;
+        network.silent(url, function(html){
+            let results = [];
+            let matches = html.matchAll(/<a class="short" href="(.*?)"[^>]*>\s*<img src="(.*?)"[^>]*>\s*<div class="short-title">([^<]*)<\/div>/g);
+            for (let match of matches) {
+                results.push({
+                    title: match[3],
+                    url: match[1].startsWith('http') ? match[1] : 'https://eneyida.tv' + match[1],
+                    quality: 'HD',
+                    info: '🇺🇦 озвучка',
+                    poster: match[2].startsWith('http') ? match[2] : 'https://eneyida.tv' + match[2]
+                });
+            }
+            call(results);
+        });
+    };
+
+    eneyida.play = function(item, call) {
+        network.silent(item.url, function(html){
+            let match = html.match(/<iframe.+?src="(.*?)"/);
+            if (match) {
+                call([{
+                    file: match[1],
+                    quality: 'HD',
+                    label: 'Eneyida',
+                    type: 'video'
+                }]);
+            } else {
+                call([]);
+            }
+        });
+    };
+
+    Lampa.Platform.addProvider({
+        name: 'eneyida',
+        version: '1.1',
+        type: 'video',
+        search: eneyida.search,
+        play: eneyida.play
+    });
+})();
